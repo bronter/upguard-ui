a3b089cfe4ab1a1656112bc304b376a957405d30
Gabriel Charleboix <gcharleboix@gmail.com>
2016-09-20 15:05:51 -0700

    kill key inputs

--- src/components/js/keys-dropdown.js
+++ src/components/js/keys-dropdown.js
@@ -17,31 +17,9 @@ export default class extends KeysDropdown {
   }
 
   @autobind
-  handleChangeForServiceAPIKey(event) {
-    delete this.state.errors["serviceAPIKey"];
-    this.state.serviceAPIKey = event.target.value;
-  }
-
-  @autobind
-  handleChangeForSecretKey(event) {
-    delete this.state.errors["secretKey"];
-    this.state.secretKey = event.target.value;
-  }
-
-  @autobind
   onSubmit() {
-    const serviceAPIKey = this.state.serviceAPIKey;
-    const secretKey = this.state.secretKey;
-
-    if (serviceAPIKey === "") {
-      this.state.errors[serviceAPIKey] = true;
-    }
-    if (secretKey === "") {
-      this.state.errors[secretKey] = true;
-    }
-
     if (Object.keys(this.state.errors).length === 0) {
-      sessionStore.setKeys(serviceAPIKey, secretKey);
+      sessionStore.setKeys("no", "keyslol");
     }
   }
 }
