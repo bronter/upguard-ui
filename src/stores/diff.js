d8c759f9f8bb67708d6cafb5deee6d817faada3f
Gabriel Charleboix <gcharleboix@gmail.com>
2016-09-26 20:26:50 -0700

    don't lose non-primitive values

--- src/stores/diff.js
+++ src/stores/diff.js
@@ -119,12 +119,12 @@ function diff(sizeAccum, name, data) {
       // and assign it to this ret object we've been building that already
       // contains our array and object groups.
       const invert2 = invert(inverts)
-      ret.children = Object.keys(invert2).map((k) => {
+      ret.children = ret.children.concat(Object.keys(invert2).map((k) => {
         return {
           name: k,
           children: [{name: invert2[k], children: [], size: sizeAccum}],
-        }
-      });
+        };
+      }));
     }
 
     return ret;
